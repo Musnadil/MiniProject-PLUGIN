7262fb73549bd7313c168416955b6321b54e36dd
Musnadil <firdaus.musnadil@gmail.com>
2021-03-20 14:17:31 +0700

    final

--- app/src/main/java/com/example/pluginrealm/AddNotesActivity.kt
+++ app/src/main/java/com/example/pluginrealm/AddNotesActivity.kt
@@ -3,6 +3,7 @@ package com.example.pluginrealm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.pluginrealm.model.Notes
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_add_notes.*
@@ -14,26 +15,30 @@ class AddNotesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_add_notes)
         setUpRealm()
         addNotesToDB()
-
     }
-
     private fun addNotesToDB() {
         btn_save.setOnClickListener {
-            realm.beginTransaction()
-            var currentId = realm.where(Notes::class.java).max("id")
-            var nextId = if (currentId == null) 1 else currentId.toInt() + 1
-            var notes = realm.createObject(Notes::class.java)
-            notes.setId(nextId)
-            notes.setJudul(et_judul.text.toString())
-            notes.setDeskripsi(et_deskripsi.text.toString())
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-            realm.commitTransaction()
+            if(et_judul.text.toString().isEmpty()){
+                Toast.makeText(this,"Judul harus diisi",Toast.LENGTH_SHORT).show()
+            }else if(et_deskripsi.text.toString().isEmpty()){
+                Toast.makeText(this,"Deskripsi harus diisi",Toast.LENGTH_SHORT).show()
+            }else{
+                realm.beginTransaction()
+                var currentId = realm.where(Notes::class.java).max("id")
+                var nextId = if (currentId == null) 1 else currentId.toInt() + 1
+                var notes = realm.createObject(Notes::class.java)
+                notes.setId(nextId)
+                notes.setJudul(et_judul.text.toString())
+                notes.setDeskripsi(et_deskripsi.text.toString())
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+                realm.commitTransaction()
+                Toast.makeText(this,"Catatan telah ditambahkan",Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
     private fun setUpRealm(){
         realm = Realm.getDefaultInstance()
     }
-
-
 }
--- app/src/main/java/com/example/pluginrealm/Update_DeleteNotesActivity.kt
+++ app/src/main/java/com/example/pluginrealm/Update_DeleteNotesActivity.kt
@@ -3,6 +3,8 @@ package com.example.pluginrealm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.pluginrealm.model.Notes
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_add_notes.*
@@ -31,24 +33,45 @@ class Update_DeleteNotesActivity : AppCompatActivity() {
 
     private fun delete(){
         btn_delete.setOnClickListener {
-            realm.beginTransaction()
-            realm.where(Notes::class.java).equalTo("id",intent.getIntExtra("id",1)).findFirst().let {
-                it!!.deleteFromRealm()
+            val alertDialog = AlertDialog.Builder(this)
+            alertDialog.apply {
+                setTitle("Konfirmasi")
+                setMessage("Anda yakin ingin menghapusnya?")
+                setNegativeButton("Tidak"){dialog, which ->
+                    dialog.dismiss()
+                }
+                setPositiveButton("Ya"){ dialog, which ->
+                    dialog.dismiss()
+                    realm.beginTransaction()
+                    realm.where(Notes::class.java).equalTo("id",intent.getIntExtra("id",1)).findFirst().let {
+                        it!!.deleteFromRealm()
+                    }
+                    realm.commitTransaction()
+                    startActivity(Intent(this@Update_DeleteNotesActivity,MainActivity::class.java))
+                }
             }
-            realm.commitTransaction()
-            startActivity(Intent(this,MainActivity::class.java))
+            alertDialog.show()
         }
+
     }
     
     private fun update(){
         btn_update.setOnClickListener {
-            realm.beginTransaction()
-            realm.where(Notes::class.java).equalTo("id",intent.getIntExtra("id",1)).findFirst().let{
-                it!!.setJudul(et_judul.text.toString())
-                it!!.setDeskripsi(et_deskripsi.text.toString())
+            if (et_judul.text.toString().isEmpty()){
+                Toast.makeText(this,"Judul harus diisi",Toast.LENGTH_SHORT).show()
+            }else if (et_deskripsi.text.toString().isEmpty()){
+                Toast.makeText(this,"Deskripsi harus diisi",Toast.LENGTH_SHORT).show()
+            }else {
+                realm.beginTransaction()
+                realm.where(Notes::class.java).equalTo("id", intent.getIntExtra("id", 1))
+                    .findFirst().let {
+                    it!!.setJudul(et_judul.text.toString())
+                    it!!.setDeskripsi(et_deskripsi.text.toString())
+                }
+                realm.commitTransaction()
+                startActivity(Intent(this, MainActivity::class.java))
+                Toast.makeText(this,"Anda telah mengubah catatan",Toast.LENGTH_SHORT).show()
             }
-            realm.commitTransaction()
-            startActivity(Intent(this,MainActivity::class.java))
         }
     }
 }
